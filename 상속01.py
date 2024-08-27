0c7dcab1535fb7b69174ff6dcf876bb74e0e7044
seoulsaram24 <seoulsaram24@gmail.com>
2024-08-27 15:36:50 +0900

    파일연습완료

--- 상속01.py
+++ 상속01.py
@@ -17,11 +17,12 @@ class Student(Person):
     def printInfo(self):
         print("Info(이름:{0}, 번호: {1})".format(self.name, self.phoneNumber))
         print("Info(학과:{0}, 학번: {1})".format(self.subject, self.studentID))
+        
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 p.printInfo()
 s.printInfo()
-#print(p.__dict__)
-#print(s.__dict__)
+# print(p.__dict__)
+# print(s.__dict__)
 
 
